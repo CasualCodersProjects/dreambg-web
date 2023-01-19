5395545ed5778ff62ed2f76f1fa30e0c0e548920
chand1012 <chandler@multisiglabs.org>
2023-01-19 17:39:32 -0500

    Add image likes

---
+++ services/likesFetcher.ts
@@ -0,0 +1,45 @@
+import { Database } from "@/types/database.types";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+export async function userLikedFetcher(supabaseClient: SupabaseClient<Database>, user_id: string | undefined, image_id: string) {
+  if (!user_id) {
+    return null;
+  }
+  const { data, error } = await supabaseClient
+    .from("likes")
+    .select("*")
+    // @ts-ignore
+    .eq("user_id", user_id)
+    // @ts-ignore
+    .eq("image", image_id)
+    .limit(1);
+
+  if (error) {
+    throw error;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  return !!data.length;
+}
+
+export async function likeCountFetcher(supabaseClient: SupabaseClient<Database>, image_id: string) {
+  const { data, error } = await supabaseClient
+    .from("likes_count")
+    .select("*")
+    // @ts-ignore
+    .eq("image", image_id)
+    .limit(1);
+
+  if (error) {
+    throw error;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  return data[0]?.likes_count || 0;
+}
