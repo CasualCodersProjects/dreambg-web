38f94154c4bdf20318ad4c8d15fd6fee0f005a6e
chand1012 <chandler@multisiglabs.org>
2023-02-27 12:11:43 -0500

    Make FAQ more subtle

--- components/modules/FAQ.tsx
+++ components/modules/FAQ.tsx
@@ -1,95 +1,65 @@
 import {
-  createStyles,
-  Title,
   Container,
+  Title,
   Accordion,
+  createStyles,
   ThemeIcon,
-  MantineProvider,
+  useMantineTheme,
 } from "@mantine/core";
-import { IconPlus } from "@tabler/icons";
+import {
+  IconPlus,
+  TablerIcon,
+  IconHeart,
+  IconStar,
+  IconPremiumRights,
+  IconQuestionMark,
+} from "@tabler/icons";
 
-const useStyles = createStyles((theme, _params, getRef) => {
-  const icon = getRef("control");
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    paddingTop: theme.spacing.xl * 2,
+    paddingBottom: theme.spacing.xl * 2,
+    minHeight: 650,
+  },
 
-  return {
-    wrapper: {
-      paddingTop: theme.spacing.xl * 2,
-      minHeight: 820,
-      backgroundImage: `radial-gradient(${theme.colors.pink[5]} 0%, ${theme.colors.grape[6]} 100%)`,
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "top left",
-      position: "relative",
-      color: theme.black,
-    },
+  title: {
+    marginBottom: theme.spacing.xl * 1.5,
+  },
 
-    title: {
-      color: theme.white,
-      fontSize: 52,
-      fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-      marginBottom: theme.spacing.xl * 1.5,
-    },
+  item: {
+    borderRadius: theme.radius.md,
+    marginBottom: theme.spacing.lg,
 
-    item: {
-      backgroundColor: theme.white,
-      borderBottom: 0,
-      borderRadius: theme.radius.md,
-      boxShadow: theme.shadows.lg,
-      overflow: "hidden",
-    },
+    border: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
 
-    control: {
-      fontSize: theme.fontSizes.lg,
-      padding: `${theme.spacing.lg}px ${theme.spacing.xl}px`,
-      color: theme.black,
+  gradient: {
+    backgroundImage: `radial-gradient(${
+      theme.colors[theme.primaryColor][7]
+    } 0%, ${theme.colors[theme.primaryColor][6]} 100%)`,
+  },
+}));
 
-      "&:hover": {
-        backgroundColor: "transparent",
-      },
-    },
-
-    content: {
-      paddingLeft: theme.spacing.xl,
-      lineHeight: 1.6,
-      color: theme.black,
-    },
-
-    icon: {
-      ref: icon,
-      marginLeft: theme.spacing.md,
-    },
-
-    gradient: {
-      backgroundImage: `radial-gradient(${
-        theme.colors[theme.primaryColor][6]
-      } 0%, ${theme.colors[theme.primaryColor][5]} 100%)`,
-    },
-
-    itemOpened: {
-      [`& .${icon}`]: {
-        transform: "rotate(45deg)",
-      },
-    },
-
-    button: {
-      display: "block",
-      marginTop: theme.spacing.md,
-
-      "@media (max-width: 755px)": {
-        display: "block",
-        width: "100%",
-      },
-    },
-  };
-});
-
-const faq: { question: string; answer: string; value: string }[] = [
+const faq: {
+  question: string;
+  answer: string;
+  value: string;
+  icon?: TablerIcon;
+  color?: string;
+}[] = [
   {
+    icon: IconHeart,
+    color: "red",
     value: "favorite-backgrounds",
     question: "How do I save my favorite backgrounds?",
     answer:
       'Simply click the "Save" button under the background you like, and it will be added to your virtual library for safe keeping. You can access your saved backgrounds at any time from your account page.',
   },
   {
+    icon: IconQuestionMark,
+    color: "blue",
     value: "why-pay-4k",
     question: "Why do I need to pay for 4K and 1440p backgrounds?",
     answer:
@@ -100,18 +70,24 @@ const faq: { question: string; answer: string; value: string }[] = [
   //   answer: "",
   // }
   {
+    icon: IconStar,
+    color: "yellow",
     value: "popular",
     question: "How do I know which backgrounds are the most popular?",
     answer:
       "User can vote on backgrounds they like by clicking the updoot icon. In the near future we will also be adding a view counter to each background.",
   },
   {
+    icon: IconPremiumRights,
+    color: "green",
     value: "commercial",
     question: "Can I use the backgrounds for commercial purposes?",
     answer:
       "DreamBG Pro members are permitted to use the image for commercial purposes as long as they are not selling the image itself. For example, you can use the image as a background for a website or stock image in a YouTube video, but you cannot sell prints or digital downloads of the image itself. If you are not a Pro member, you can use the image for personal use only.",
   },
   {
+    icon: IconPlus,
+    color: "teal",
     value: "make-my-own",
     question: "Can I make my own backgrounds?",
     answer:
@@ -121,42 +97,50 @@ const faq: { question: string; answer: string; value: string }[] = [
 
 export function FAQ() {
   const { classes } = useStyles();
-  return (
-    <MantineProvider inherit theme={{ colorScheme: "light" }}>
-      <div className={classes.wrapper}>
-        <Container size="sm">
-          <Title align="center" className={classes.title}>
-            Frequently Asked Questions
-          </Title>
+  const theme = useMantineTheme();
+  const getColor = (color: string) =>
+    theme?.colors?.[color]?.[theme.colorScheme === "dark" ? 6 : 7];
 
-          <Accordion
-            chevronPosition="right"
-            defaultValue="favorite-backgrounds"
-            chevronSize={50}
-            variant="separated"
-            disableChevronRotation
-            chevron={
-              <ThemeIcon radius="xl" className={classes.gradient} size={32}>
-                <IconPlus size={18} stroke={1.5} />
-              </ThemeIcon>
-            }
-            py="sm"
-          >
-            {faq.map((item, key) => {
-              return (
-                <Accordion.Item
-                  key={key}
-                  className={classes.item}
-                  value={item.value}
-                >
-                  <Accordion.Control>{item.question}</Accordion.Control>
-                  <Accordion.Panel>{item.answer}</Accordion.Panel>
-                </Accordion.Item>
-              );
-            })}
-          </Accordion>
-        </Container>
-      </div>
-    </MantineProvider>
+  return (
+    <Container size="lg" className={classes.wrapper}>
+      <Title align="center" className={classes.title}>
+        Frequently Asked Questions
+      </Title>
+
+      <Accordion
+        styles={{
+          chevron: {
+            "&[data-rotate]": {
+              transform: "rotate(45deg)",
+              transition: "transform 250ms ease",
+            },
+          },
+        }}
+        chevron={
+          <ThemeIcon radius="xl" className={classes.gradient}>
+            <IconPlus size={18} />
+          </ThemeIcon>
+        }
+        variant="separated"
+      >
+        {faq.map((item, key) => (
+          <Accordion.Item key={key} className={classes.item} value={item.value}>
+            <Accordion.Control
+              icon={
+                item?.icon && (
+                  <item.icon
+                    size={20}
+                    color={getColor(item?.color || "pink")}
+                  />
+                )
+              }
+            >
+              {item.question}
+            </Accordion.Control>
+            <Accordion.Panel>{item.answer}</Accordion.Panel>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </Container>
   );
 }
